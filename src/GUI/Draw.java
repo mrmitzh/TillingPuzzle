fe074e66cbc84e70933619a918d1bf8c3cddf765
mrmitzh <tianzhanhong412@gmail.com>
2019-12-04 00:24:36 -0500

    add more color selection

--- src/GUI/Draw.java
+++ src/GUI/Draw.java
@@ -10,6 +10,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Draw extends Component {
@@ -23,9 +25,44 @@ public class Draw extends Component {
     private ReadFile readFile;
     private JPanel showAllTiles;
     private JPanel showTileSolution;
+    private ArrayList<Color> colorMapping;
+    private List<Tile> tiles;
 
-    public Draw()
+    public Draw(List<Tile> tiles)
     {
+        this.tiles = tiles;
+        colorMapping = new ArrayList<>();
+        colorMapping.addAll(Arrays.asList(Color.red, Color.green, Color.blue,
+                Color.yellow, Color.cyan, new Color(46, 139, 87),
+                new Color(148, 0, 211), new Color(135, 51, 36), Color.magenta,
+                Color.gray, Color.pink, new Color(175, 255, 225),
+                new Color(130, 175, 190)));
+        int[] H = new int[] {0, 20, 30, 40, 50, 60, 90, 160, 190,
+                205, 220, 235, 260, 285, 305, 330};
+        int[] S = new int[] {100, 50, 100, 75, 100};
+        int[] B = new int[] {100, 100, 70, 100, 85};
+        float h, s, b;
+        int i = 0;
+        while (colorMapping.size() < tiles.size()) {
+            List<Color> color = new ArrayList<Color>();
+            for (int j = 0; j < H.length; j++) {
+                // skip some indistinct colors
+                if (i == 0 && (j == 0 || j == 3 || j == 5 || j == 11 || j == 14)) continue;
+                if (i == 1 && (j == 1 || j == 2 || j == 3 || j == 4)) continue;
+                if (i == 2 && (j == 10 || j == 11 || j == 12 || j == 13)) continue;
+                if (i == 4 && (j == 11 || j == 12)) continue;
+                h = H[j] / 360.f;
+                s = S[i % S.length] / 100.f;
+                b = B[i % B.length] / 100.f;
+                color.add(Color.getHSBColor(h, s, b));
+            }
+            Collections.shuffle(color);
+            colorMapping.addAll(color);
+            i++;
+        }
+
+
+
         jFrame = new JFrame();
         menuBar = new JMenuBar();
         menu = new JMenu("File");
@@ -161,7 +198,8 @@ public class Draw extends Component {
     }
 
 
-    private void showTileList(List<Tile> tiles)
+
+    private void showTileList()
     {
         ArrayList<JPanel> allPanelList = new ArrayList<>();
         int counter = 0;
@@ -172,23 +210,10 @@ public class Draw extends Component {
             currentPanel.setLayout(new GridLayout(tile.data.length,tile.data[0].length));
             for(int i = 0; i < tile.data.length;i++)
             {
-                for(int j = 0; j < tile.data[0].length;j++)
-                {
-                    if(tile.data[i][j] != ' ')
-                        System.out.print(tile.data[i][j]);
-                    else
-                        System.out.print("@");
-                }
-                System.out.println();
-            }
-            System.out.println();
-            System.out.println();
-            for(int i = 0; i < tile.data.length;i++)
-            {
                 for(int j = 0; j < tile.data[i].length;j++)
                 {
                     JPanel temp = new JPanel();
-                    temp.setBackground(Color.WHITE);
+                    temp.setBackground(colorMapping.get(counter-1));
                     if(tile.data[i][j] != ' ')
                     {
                         // has data
@@ -219,8 +244,8 @@ public class Draw extends Component {
     public static void main(String[] args)
     {
         ReadFile readFile = new ReadFile(args[0]);
-        Draw draw = new Draw();
-        draw.showTileList(readFile.tiles);
+        Draw draw = new Draw(readFile.tiles);
+        draw.showTileList();
     }
 
 }
